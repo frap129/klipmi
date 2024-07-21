317d35ac2360eb382c444bb3f45eba326d5b38e2
Joe Maples <joe@maples.dev>
2024-07-21 10:32:17 -0400

    fix(install): Check EUID first

--- install.sh
+++ install.sh
@@ -3,6 +3,14 @@
 KLIPMI_DIR=$(dirname $(realpath $0))
 SYSTEMD_DIR="/etc/systemd/system"
 
+root_check() {
+    if [[ "$EUID" -eq 0 ]]; then
+        echo "Do not run this script as root."
+        echo "You will be prompted for your password when root access is needed."
+        exit 1
+    fi
+}
+
 install_klipmi_service() {
     sudo cp ${KLIPMI_DIR}/klipmi.service ${SYSTEMD_DIR}/klipmi.service
     sudo sed -i "s|%USER%|${USER}|g; s|%KLIPMI_DIR%|${KLIPMI_DIR}|;" ${SYSTEMD_DIR}/klipmi.service
@@ -14,5 +22,6 @@ create_klipmi_venv() {
     ${KLIPMI_DIR}/.venv/bin/pip install -r ${KLIPMI_DIR}/requirements.txt
 }
 
+root_check
 create_klipmi_venv
 install_klipmi_service
